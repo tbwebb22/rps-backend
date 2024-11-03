14ee0150ce56c5c86e726dd6c64feb2ed684423c
Taylor Webb <tbwebb22@gmail.com>
2024-11-03 16:43:02 -0500

    add auth

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import gameRoutes from './routes/gameRoutes';
 import dotenv from 'dotenv';
 import { init } from "@airstack/node";
+import { authenticateApiKey } from './middleware/auth';
 
 dotenv.config();
 init(process.env.AIRSTACK_API_KEY || "");
 
 const app: Express = express();
 app.use(bodyParser.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'POST' || req.method === 'GET') {
+      authenticateApiKey(req, res, next);
+    } else {
+      next();
+    }
+  });
 
 app.use('/api/games', gameRoutes);
 
