ed3e326cca82f23fcdc3f3197c282caa3bce25a1
Taylor Webb <tbwebb22@gmail.com>
2024-11-17 18:02:04 -0500

    env vars for both local and remote

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -4,11 +4,24 @@ import { Database } from './database.types'
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process.env.NODE_ENV === 'development'
+    ? process.env.LOCAL_SUPABASE_URL
+    : process.env.SUPABASE_URL
+const supabaseKey = process.env.NODE_ENV === 'development'
+    ? process.env.LOCAL_SUPABASE_KEY
+    : process.env.SUPABASE_KEY
 
-if (!supabaseUrl || !supabaseKey) {
+if (process.env.NODE_ENV === 'development') {
+  if (!process.env.LOCAL_SUPABASE_URL || !process.env.LOCAL_SUPABASE_KEY) {
+    throw new Error('Missing Supabase URL or Key')
+  }
+} else {
+  if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase URL or Key')
+  }
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+export const supabase = createClient<Database>(
+    supabaseUrl!,
+    supabaseKey!
+)
